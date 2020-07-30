5beaed72f4053cf640a3d5ab6c5825ffc8fc4d33
mah.militello <mmauff@gmail.com>
2020-07-30 15:34:19 -0300

    alguns exercicios prontos

---
+++ semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,48 @@
+/*EXERCÍCIO 1
+O código vai somando +1 ao valor de i (que começa em 0), consequentemente 
+alterando o valor da variável -valor- num loop até que o número somado a i 
+seja menor que 5 (ou seja, 4). Então, o resultado de -valor- final que será
+impresso é 10. */
+
+/*EXERCÍCIO 2
+a) Os números maiores que 18 serão impressos.
+b) Sim, apenas eliminar a linha com a condição if, assim ele imprime todos os
+índices da lista.*/
+
+/*EXERCÍCIO 3:
+
+a) const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+for (let numero of array) {
+		console.log(numero)
+    }
+    
+b) const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+for (let numero of array) {
+		console.log(numero / 10)
+    }
+
+
+c) const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+   const novaArray = []
+
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+        novaArray.push(array[i])
+    }
+}
+console.log(novaArray)
+
+d) const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+const novaArray = []
+
+for (let i = 0; i < array.length; i++) {
+    novaArray.push("O elemento do index " + i + " é " + array[i])
+ }
+for (let numero of novaArray) {
+		console.log(numero)
+	}
+
+    */
+
