6016d37f50a163735294d4ec54b85028a8c06eeb
mah.militello <mmauff@gmail.com>
2020-08-08 00:00:28 -0300

    interpretação de código ok

---
+++ semana4/lista-de-exercicios/exercicio.js
@@ -0,0 +1,37 @@
+//----------- EXERCICIOS DE INTERPRETACAO DE CODIGO ------------
+
+/* 1 - Através da função conversorDeMoeda, o código calcula o valor em reais a partir
+do valor da cotação do dólar informado pelo usuário (cast de string para number) 
+multiplicado pelo valor em dólar atribuído ao parâmetro da função. Será impresso no console o
+resultado da operação atribuído à variável meuDinheiro.*/
+                //uso do return segue obscuro e aqui está dentro da chave
+                //testei e tudo ok
+
+/* 2 - Através da função investeDinheiro, o código calcula o valor do montante de 
+Dinheiro após investimento, usando o comando switch case para condicionais para calcular 
+o valor após investimento a partir do tipo de investimento e o valor investido 
+informados como parâmetros da função. Ainda há um alerta emitido quando o campo para 
+informar o tipo de investimento estiver incorreto. A função foi chamada duas vezes 
+e seus valores foram atribuídos a variáveis e impressos no console.*/ 
+                //uso do return segue obscuro e aqui ele aparece fora da chave
+                /*testei e imprimiu undefined no segundo console após o alert, mas entendi:
+                 "Tesouro Direto" não era um case da condicional*/ 
+               
+
+/* 3 - Através de um loop for e uma condicional if, os números pares da array numeros são 
+adicionados à array1 e caso não sejam pares, são adicionados à array2 com o .push. 
+A quantidadde de elementos de cada uma das três arrays são impressas no console 
+através do .length. */
+                //testei e tudo ok
+
+/* 4 - Através do loop for, o código verifica cada número da array numeros. Se o número (guardado 
+na variável numero) for menor que infinito (valor atribuído à variável numero1), a variável 
+numero1 passa a ter o valor da variável numero. E em outra condicional, se o número for 
+maior que zero (valor atribuído à variável numero2), a variável numero2 passa a ter o valor
+da variavel numero. É esperado que todos os números sejam impressos com o console.log da 
+variável numero1 e todos os números (menos o -10) sejam impressos com o console.log da 
+variável numero2. */ 
+                //testei e imprimiu -10 e 1590 - QUE???
+
+
+
