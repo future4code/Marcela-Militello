c4670a5627d69a36b3e3d2c3b5332728c4467c24
mah.militello <mmauff@gmail.com>
2020-08-10 14:32:36 -0300

    segundo bloco quase finalizado

--- semana4/lista-de-exercicios/exercicio.js
+++ semana4/lista-de-exercicios/exercicio.js
@@ -1,4 +1,4 @@
-//----------- EXERCICIOS DE INTERPRETACAO DE CODIGO ------------
+//----------- Exercícios de interpretação de código ------------
 
 /* 1 - Através da função conversorDeMoeda, o código calcula o valor em reais a partir
 do valor da cotação do dólar informado pelo usuário (cast de string para number) 
@@ -31,7 +31,84 @@ maior que zero (valor atribuído à variável numero2), a variável numero2 pass
 da variavel numero. É esperado que todos os números sejam impressos com o console.log da 
 variável numero1 e todos os números (menos o -10) sejam impressos com o console.log da 
 variável numero2. */ 
-                //testei e imprimiu -10 e 1590 - QUE???
+                /*testei e imprimiu -10 e 1590, mas entendi depois da explicação de um colega:
+                o código analisa caso a caso (loop) e só imprime o resultado final (console.log
+                fora das chaves)
+    
 
 
+//------------- Exercícios de Lógica de Programação -------------
 
+/* 1 - Através de while, for e for of.
+
+let i = 0
+
+while (i < 10) {
+    console.log(i)
+    i++
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let numero of numeros){
+    console.log(numero)
+}
+*/
+
+/* 2 - 
+a) true
+b) true
+c) true
+d) true 
+e) true
+*/
+
+/* 3 - Não funciona, pois falta atribuir um valor à const e retirar o = de dentro
+da condição.
+
+const quantidadeDeNumerosPares = (prompt ("Digite um número"))
+
+let i = 0
+while(i < quantidadeDeNumerosPares) {
+    console.log(i*2)
+    i++
+}
+*/
+
+/* 4 - 
+
+function classificacaoTriangulo (a, b, c){
+    if (a === b && b === c){
+        console.log("Equilátero")
+    } else if (a !== b && b !== c && c !== a) {
+        console.log("Escaleno")
+    } else { 
+        console.log("Isósceles")
+    }
+}
+
+classificacaoTriangulo(a, b, c) //inserir números no lugar dos parâmetros
+*/
+
+/* 5 - */
+
+let numero1 = prompt ("Digite o primeiro número")
+let numero2 = prompt ("Digite o segundo número")
+
+if (numero1 > numero2) {
+    console.log ("O maior é: " + numero1)
+} else if (numero2 > numero1) { 
+    console.log ("O maior é: " + numero2)
+} else {
+    console.log ("Números iguais!")
+}
+
+if (numero1 % numero2 === 0){
+    console.log (numero1 + " é divisível por " + numero2)
+} else {
+    console.log (numero1 + " não é divisível por " + numero2)
+}
