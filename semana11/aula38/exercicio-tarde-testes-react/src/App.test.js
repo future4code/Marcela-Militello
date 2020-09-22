c77eee8173b978f691ef76b8fdc3a027f3f8d12c
mah.militello <mmauff@gmail.com>
2020-09-22 17:47:02 -0300

    item 1 ok

--- semana11/aula38/exercicio-tarde-testes-react/src/App.test.js
+++ semana11/aula38/exercicio-tarde-testes-react/src/App.test.js
@@ -2,18 +2,15 @@ import React from "react";
 import { render, fireEvent, getByPlaceholderText } from "@testing-library/react";
 import App from "./App";
 
-// const listaPost = () => {
-//     return (
-//         <Post />
-//     )
-// }
-
 test("Ao digitar no input e clicar no botão Adicionar, um novo post é criado", () => {
     const { getByPlaceholderText, getByText } = render(<App />);
     
     const inputNovoPost = getByPlaceholderText(/Novo Post/i);
     const botaoAdicionar = getByText(/Adicionar/i);
 
-    fireEvent.change(inputNovoPost), { target: { value: "inputTest"}};
+    fireEvent.change(inputNovoPost, { target: { value: "inputTest"}});
     fireEvent.click(botaoAdicionar);
+
+    let post = getByText(/inputTest/i);
+    expect(post).toHaveTextContent("inputTest")
 })
