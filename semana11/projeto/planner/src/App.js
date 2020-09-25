9acc20c9077738f97f0c4a80ab041b590a4cc071
mah.militello <mmauff@gmail.com>
2020-09-25 17:45:59 -0300

    funcionalidades ok

--- semana11/projeto/planner/src/App.js
+++ semana11/projeto/planner/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import { useAxiosConfigs } from './axios/AxiosConfigs';
+import Planner from './components/Planner';
 
 function App() {
+  const { getTasks, tasks } = useAxiosConfigs()
+
   return (
-    <div> </div>
+    <main className="App">
+      <Planner getTasks={getTasks} tasks={tasks} />
+    </main>
   );
 }
 
