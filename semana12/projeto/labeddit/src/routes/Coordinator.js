57766226c6059d039d4c39eb6cb623a4e180a5a9
mah.militello <mmauff@gmail.com>
2020-09-28 17:13:53 -0300

    projeto iniciado com esqueleto

---
+++ semana12/projeto/labeddit/src/routes/Coordinator.js
@@ -0,0 +1,15 @@
+export const goToLogin = (history) => {
+    history.push('/login')
+}
+
+export const goToSignUp = (history) => {
+    history.push('/signup')
+}
+
+export const goToFeed = (history) => {
+    history.push('/feed')
+}
+
+export const goToPosts = (history) => {
+    history.push('/posts')
+}
---
+++ semana12/projeto/labeddit/src/routes/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginPage from '../screens/LoginPage/LoginPage';
+import PostPage from '../screens/PostPage/PostPage';
+import SignUpPage from '../screens/SignUpPage/SignUpPage';
+import FeedPage from '../screens/FeedPage/FeedPage';
+import ErrorPage from '../screens/ErrorPage/ErrorPage';
+
+const Router = () => {
+    return (
+        <Switch>
+            <Route exact path={'/login'}>
+                <LoginPage/>
+            </Route>
+            <Route exact path={'/signup'}>
+                <SignUpPage/>
+            </Route>
+            <Route exact path={'/feed', '/'}>
+                <FeedPage/>
+            </Route>
+            <Route exact path={'/posts'}>
+                <PostPage/>
+            </Route>
+            <Route>
+                <ErrorPage/>
+            </Route>
+        </Switch>        
+    )
+}
+
+export default Router
+
